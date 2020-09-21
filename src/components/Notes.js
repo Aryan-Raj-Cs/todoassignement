a734cff2ebd9ba3fd4dbd7ae85d101acd0fe093e
Aryan-Raj-Cs <aryanrajresonance@gmail.com>
2020-09-21 15:13:35 +0530

    super final

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -9,27 +9,34 @@ import Checkbox from '@material-ui/core/Checkbox';
 const Notes = (props) => {
     const [count,setCount]=useState(0)
     //console.log(props.obj)
+    const [show,setShow]=useState(false)
     const { state, dispatch } = useContext(userContext);
-    const complete =(date)=>{
+    const complete =(date,v)=>{
         let newstate=[]
         if(state.length!=0){
         newstate = state.map(val=>{
+          if(v==1){
            if(val.date==date && !val.completed){
                val.completed=true;
                val.completedtime=Date.now();
                
            }
+        }
+        
            return val;
        })
 
     }
     else{
          newstate = JSON.parse(localStorage.getItem("notes")).map(val=>{
+            if(v==1){
             if(val.date==date && !val.completed){
                 val.completed=true;
                 val.completedtime=Date.now();
                 
             }
+        }
+        
             return val;
         })
   
@@ -47,9 +54,12 @@ const Notes = (props) => {
     }
 
     const search=(e,data)=>{
-       // e.stopPropagation();
+        e.stopPropagation();
        // console.log(data)
+      // e.preventDefault()
+      complete(props.obj.date,0)
         props.showhastag(data)
+        setShow(true)
        
     }
     const hashtag=()=>{
@@ -73,7 +83,7 @@ const Notes = (props) => {
 
             <div className="notes" 
             
-            onClick={()=>{complete(props.obj.date)}}
+            onClick={()=>{complete(props.obj.date,1)}}
             >
 
                 
