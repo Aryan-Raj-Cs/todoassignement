43debea0cc35aede39aa5e4c635bde6c171a2d33
Aryan-Raj-Cs <aryanrajresonance@gmail.com>
2020-09-21 14:28:37 +0530

    Updates

---
+++ precache-manifest.4741cd765b9d41d87025df0541b9f8b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddbaa727551a0ca2353bedc0d8d745e",
+    "url": "/todohosted/index.html"
+  },
+  {
+    "revision": "29530777ffd5f86d02aa",
+    "url": "/todohosted/static/css/main.42221896.chunk.css"
+  },
+  {
+    "revision": "2d0eb291243e53232208",
+    "url": "/todohosted/static/js/2.6f786271.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todohosted/static/js/2.6f786271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29530777ffd5f86d02aa",
+    "url": "/todohosted/static/js/main.d5aec142.chunk.js"
+  },
+  {
+    "revision": "b9745889863588f0674e",
+    "url": "/todohosted/static/js/runtime-main.13093d37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todohosted/static/media/logo.5d5d9eef.svg"
+  }
+]);
